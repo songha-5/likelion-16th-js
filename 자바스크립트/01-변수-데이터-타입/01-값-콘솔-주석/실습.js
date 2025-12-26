657d794501b95c040697762a5b9b6c7ef8fca471
songha-5 <shn0518@gmail.com>
2025-12-26 13:39:31 +0900

    구학습:  javascript 데이터 타입(숫자, 문자, 불리언)

--- 자바스크립트/01-변수-데이터-타입/01-값-콘솔-주석/실습.js
+++ 자바스크립트/01-변수-데이터-타입/01-값-콘솔-주석/실습.js
@@ -1,12 +1,26 @@
-숫자(number) 값
+// 숫자(number value or literal) 값
+1;
+2;
+3;
+// 하나
+// two
 
-문자(strung) 값
+// 문자(strung) 값
+// 작은따옴표 (')
+("2px");
+// 2px -> 그대로 쓰면 문법오류 발생
+// 큰따옴표(")
+("2mm");
+// -> 오류발생 ";"가 없기때문
+// 프로그램 문장이 끝날 때 마침을 의미하는 세미콜론추가
+// 역따옴표(`)
+`2inch`;
 
-논리(boolean) 값
+// 논리(boolean) 값
 
-터미널 값 출력
+// 터미널 값 출력
 
-주석 한 줄(single line comment)
+// 주석 한 줄(single line comment)
 
-여러 줄 
-주석 작성 (multi line comment)
+/* 여러 줄 
+주석 작성 (multi line comment) */
