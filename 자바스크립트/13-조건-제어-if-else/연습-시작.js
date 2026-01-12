dc6d56882895da6d187a574d9cae8861b9dece17
songha-5 <shn0518@gmail.com>
2026-01-12 16:55:11 +0900

    학습: 조건-제어 실습문제 풀이

--- 자바스크립트/13-조건-제어-if-else/연습-시작.js
+++ 자바스크립트/13-조건-제어-if-else/연습-시작.js
@@ -10,7 +10,12 @@
 //    - 그 외의 경우 '미성년자입니다.' 출력
 
 // [작성 공간]
-let age
+let age = 17
+if(age > 18) {
+  console.log('성인입니다.')
+} else {
+  console.log('미성년입니다.')
+}
 
 
 // --------------------------------------------------------------------------
@@ -27,7 +32,16 @@ let age
 //    - 그 외의 경우 '학점: F' 출력
 
 // [작성 공간]
-let score
+let score = 85
+if (score >= 90) {
+  console.log('학점: A')
+} else if (score >= 80 && score < 90) {
+  console.log('학점: B')
+} else if (score >= 70 && score < 80) {
+  console.log('학점: C')
+} else {
+  console.log('학점: F')
+}
 
 
 // --------------------------------------------------------------------------
@@ -44,4 +58,12 @@ let score
 // 3. userInput 변수를 null로 변경하고 다시 테스트
 
 // [작성 공간]
-let userInput
+let userInput = null
+
+if(userInput === null) {
+  console.log('입력 값이 없습니다.')
+} else if (userInput === 0) {
+  console.log('입력값이 0입니다.')
+} else {
+  console.log('입력 값:'+userInput)
+}
