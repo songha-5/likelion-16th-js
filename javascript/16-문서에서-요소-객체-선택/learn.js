093f9440ecdbfbebb264d8b0116e1f6615a16c92
songha-5 <shn0518@gmail.com>
2026-01-14 15:24:49 +0900

    학습: 다양한 웹 API를 사용해 문서의 요소 찾기 실습

--- javascript/16-문서에서-요소-객체-선택/learn.js
+++ javascript/16-문서에서-요소-객체-선택/learn.js
@@ -103,6 +103,39 @@ console.log(allMusicicans) // NodeList(3) [li, li, li] < 유사배열객체
 const emails = document.getElementsByName('email')
 console.log(emails)
 
+// 1. id 속성으로 DOM 요소를 선택하여 콘솔 패널에 출력해봅니다.
+/* const exorcistChracters = document.getElementById('exorcist-characters')
+console.log(exorcistChracters)
+// 2. id 속성으로 선택한 DOM 요소에서 다음을 수행하세요.
+//     1. 태그 또는 클래스 이름, 속성 이름/값 등을 사용해 "박요한"을 선택해보세요.
+const backYoHan = document.querySelector('.yo-han')
+const backYoHanTag = exorcistChracters.getElementsByTagName('li').item(0)
+console.log(backYoHan)
+console.log(backYoHanTag)
+//     2. 태그 또는 클래스 이름, 속성 이름/값 등을 사용해 "최강림"을 선택해보세요.
+const choiKangRim = document.querySelector('.master-choi')
+console.log(choiKangRim)
+//     3. 태그 또는 클래스 이름, 속성 이름/값 등을 사용해 "악령"을 선택해보세요.
+const villanin = document.querySelector('[data-type *="villain"]')
+console.log(villanin)
+ */
+const exorcistCharacters = document.getElementById('exorcist-characters')
+// console.log(exorcistCharacters)
+
+// 2. id 속성으로 선택한 DOM 요소에서 다음을 수행하세요.
+//     1. 태그 또는 클래스 이름, 속성 이름/값 등을 사용해 "박요한"을 선택해보세요.
+//     1-1. 태그 이름으로 찾기
+let yohan = null
+yohan = exorcistCharacters.getElementsByTagName('li').item(0)
+//     1-2. 선택자로 찾기
+yohan = exorcistCharacters.querySelector('li') // 'li:first-child'
+//     1-3. 클래스 이름으로 찾기
+yohan = exorcistCharacters.getElementsByClassName('character').item(0)
+//     1-4. 속성 이름과 값으로 찾기 (class, data-type)
+yohan = exorcistCharacters.querySelector('[class="character yo-han"]')
+yohan = exorcistCharacters.querySelector('[data-type="hero"]')
+
+console.log(yohan) // <li> (HTML Code) → HTMLLiElement (DOM Object)
 // --------------------------------------------------------------------------
 // 그 밖의 요소 선택 API (참고)
 // --------------------------------------------------------------------------
