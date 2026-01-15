53e1d9e59bdc0c0b532f168f889b269c80a3610e
songha-5 <shn0518@gmail.com>
2026-01-15 15:29:43 +0900

    학습: 이벤트 모델 리마인드

--- javascript/18-이벤트-리스너/learn.js
+++ javascript/18-이벤트-리스너/learn.js
@@ -7,75 +7,75 @@
 // --------------------------------------------------------------------------
 
 // 1. 대상 정하기 (보초병이 지킬 성문 선택)
-// const clickableButton = document.querySelector('clickable')
-
-// 2. 이벤트 리스닝 (보초병 배치 및 귀 기울이기)
-// console.log(typeof clickableButton.addEventListener)
-// clickableButton.addEventListener('click', function() {
-  // 연습: gateButton에 'click' 이벤트 리스너를 추가하고 콘솔에 메시지를 출력하세요.
-  // console.log('HI')
-// })
-
-// 1. 대상 정하기 (보초병이 지킬 성문 선택)
 const gateButton = document.querySelector('.clickable')
 // console.log(clickableButton)
 
 // 전역에서 접근 가능하도록 공개
-// globalThis.gateButton = gateButton
+// BOM (브라우저 객체 모델: 루트 객체 → window)
+// 브라우저 콘솔 패널에서 접근하려면 전역에 공개되어야 한다.
+window.gateButton = gateButton
 
 // 2. 이벤트 리스닝 (보초병 배치 및 귀 기울이기)
-// gateButton.addEventListener('click', function() {
+gateButton.addEventListener('click', () => {
   // 연습: gateButton에 'click' 이벤트 리스너를 추가하고 콘솔에 메시지를 출력하세요.
-  // console.log('🚪 성문을 두드렸어요!')
-// })
-// 전역
-// const mood = '호기심'
+  console.log('🚪 성문을 두드렸어요!')
+})
 
-// 지역
-function localScope() {
-    const mood = '진취적 기상'
-    // 지역 변수는 외부에서 접근 가능하지 않음 ⚠️
-    // 그런데 외부에서도 내부의 변수에 접근하고 싶다면?
-    // 그럼 외부에 공개(노출)하자!
-    const whoAmI = { name: '야무' } 
-    // 전역에서 접근 가능하게 공개하려면?
-    // 전역 객체의 속성으로 만들면 된다.
-    // 전역객체.속성 = 지역변수
-    window.whoAmI = whoAmI
-    
-    // 내부에서는 접근 가능
-    console.log(whoAmI)
-}
-
-console.log(window.whoAmI)
-
-localScope()
 // --------------------------------------------------------------------------
 // 실습: 이벤트로 DOM 조작 (클래스 토글 마법)
 // --------------------------------------------------------------------------
 
 // 시나리오: 버튼을 클릭할 때마다 배경색(body)과 버튼색을 반전시킵니다.
+
 const clickedClassName = 'is-clicked'
+const body = document.body // HTMLBodyElement
 
 // [연습 1] 조건문을 사용하여 클래스 'is-clicked'를 수동으로 넣고 빼보세요.
-gateButton.addEventListener('click', function() {
-  const hasClickedClassName = gateButton.classList.contains(clickedClassName)
-  if (hasClickedClassName) {
-    gateButton.classList.remove(clickedClassName)
-  } else {
-    gateButton.classList.add(clickedClassName)
-  }
-})
+// gateButton.addEventListener('click', () => {
+//   const hasClickedClassName = gateButton.classList.contains(clickedClassName)
+//   if(hasClickedClassName) {
+//     gateButton.classList.remove(clickedClassName)
+//     body.classList.remove(clickedClassName)
+//   } else {
+//     gateButton.classList.add(clickedClassName)
+//     body.classList.add(clickedClassName)
+//   }
+// })
 
 // [연습 2] 위 로직을 단축 주문인 toggle()로 한 줄씩 작성해 보세요.
-gateButton.addEventListener('click', (e) => {
+
+// 인라인 함수를 사용한 예시
+// - 재사용이 어렵다.
+// gateButton.addEventListener('click', (e) => {
+//   gateButton.classList.toggle(clickedClassName)
+//   body.classList.toggle(clickedClassName)
+// })
+
+// 함수와 이벤트 리스너 등록 코드를 분리한 예시
+
+// 함수 = 객체 = 값 취급  
+// 변수 = 함수값 ✅
+// 분리의 장점은 재사용이 쉽다.
+const handleClick = (e) => {
+  // 이벤트 리스너(addEventListener로 연결된 함수)는
+  // 이벤트 객체를 전달받습니다.
   console.dir(e)
+
   gateButton.classList.toggle(clickedClassName)
-})
+  body.classList.toggle(clickedClassName)
+}
+
+// - 함수실행(함수_또는_함수참조변수_전달)
+// - addEventListener('type', (e) => {})
+// - addEventListener('type', handleClick)
+gateButton.addEventListener('click', handleClick)
+
 
 // --------------------------------------------------------------------------
 // 실습: 리스너 연결 상태 확인 (브라우저 콘솔용, 웹표준 아님)
 // --------------------------------------------------------------------------
+
+// console.log(getEventListeners) ❌ ReferenceError
 
 
 // --------------------------------------------------------------------------
