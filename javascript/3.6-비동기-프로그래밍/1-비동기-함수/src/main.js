f1bb37141eaed5581805c457e0374dbfc46d725b
songha-5 <shn0518@gmail.com>
2026-03-06 19:34:29 +0900

    학습: Promise 이해하기

--- javascript/3.6-비동기-프로그래밍/1-비동기-함수/src/main.js
+++ javascript/3.6-비동기-프로그래밍/1-비동기-함수/src/main.js
@@ -11,16 +11,27 @@ document.addEventListener('click', (e) => {
   if (!button) return
 
   // TODO 4: 데이터를 가져오는 함수 호출
+  console.log('데이터 페칭')
+  fetchData()
 })
 
 // --------------------------------------------------------------------------
 // TODO 1: Promise를 사용해 대기 시간을 설정하는 지연(delay) 함수 작성
-function wait() {}
+function wait(delay = 1000) {
+  // promise 객체 생성(new) 반환
+  const promise = new Promise((resolve) => {
+    // 지연처리(Timer API)
+    setTimeout(resolve, delay)
+  })
+
+  // 생선된 Promise 객체 반환
+  return promise
+}
 
 // --------------------------------------------------------------------------
 // TODO 2: fetchData 함수 코드를 비동기 함수로 작성
 function fetchData() {
-  loadingState(true)
+  loadingState(true) // 로딩중 표시
 
   simpleFetch()
     .then((data) => {
@@ -35,9 +46,11 @@ function fetchData() {
 // --------------------------------------------------------------------------
 // TODO 3: simpleFetch 함수 코드를 비동기 함수로 작성
 function simpleFetch() {
+  // fetch() => Promise 객체 반환
   return fetch(`${API_BASE_URL}/users/1`).then((response) => response.json())
 }
 
+// 화면에 로딩 상태를 처리하는 함수(기능)
 function loadingState(state = true) {
   if (state) {
     loading.hidden = false
@@ -50,6 +63,7 @@ function loadingState(state = true) {
   }
 }
 
+// 서버에서 전달받은 데이터를 화면에 그리기 함수(기능)
 function render(data) {
   container.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`
 }
